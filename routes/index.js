760517707088e51d1205ce32073fcd7879a45db7
shabarishramaswamy <shabarishramaswamy@gmail.com>
2021-10-16 19:15:26 +0530

    Added Basic Routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/**
+ * @Method - GET
+ */
+ router.post('/login', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+
+/**
+ * @Method - GET
+ */
+ router.post('/register', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+/**
+ * @Method - GET
+ */
+router.get('/user', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+
+/**
+ * @Method - GET
+ */
+ router.post('/user/register', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+
+/**
+ * @Method - GET
+ */
+ router.post('/dashboard', function(req, res, next) {
+  res.send('dashboard');
+});
+
+
+/**
+ * @Method - GET
+ */
+ router.post('/user/:userid/uploaded', function(req, res, next) {
+  res.send('index');
 });
 
 module.exports = router;
